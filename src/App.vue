3a01914a39d7df916e599cc0366cddde021978e3
Ivan Constantino <icaivan11@gmail.com>
2017-04-14 01:35:43 -0700

    deleted console tests

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,6 @@ export default {
     translateText:function(text, language){
       this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170413T082930Z.49ee9d7151ef21d3.dc42044f22486fac476bc7906dc1839a1c57c2d9&lang='+ language +'&text='+ text)
       .then((response) => {
-        console.log(response.body.text[0]);
         this.translatedText = response.body.text[0];
       });
     }
